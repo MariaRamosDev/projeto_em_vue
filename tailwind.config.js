fc3ddb5568b9dc35bfa1c43592553d61a57aaa3d
igorbkuhl <igorb.kuhl@gmail.com>
2023-10-13 07:15:21 -0300

    Configuração do tailwind adicionado.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import ('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      franklin: ["Franklin Gothic Medium", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
