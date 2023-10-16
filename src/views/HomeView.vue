82dd6c089743de025feb3b44809335a2e491d8dc
igorbkuhl <igorb.kuhl@gmail.com>
2023-10-16 16:06:54 -0300

    Arquivo HelloWorld renomeado para MainPage.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld />
+    <MainPage />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue";
+import MainPage from "@/components/MainPage.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+    MainPage,
   },
 });
 </script>
