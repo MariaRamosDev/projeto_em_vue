75ef5a5c636323cb7eba2bb2ad3f61fe0278a7d6
igorbkuhl <igorb.kuhl@gmail.com>
2023-10-18 21:35:49 -0300

    Prefixo adicionado para evitar conflitos com Bootstrap.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  prefix: "tw-",
   theme: {
     extend: {},
     colors: {
