4df70260b308a264ba977d756f621ac8b92659f7
igorbkuhl <igorb.kuhl@gmail.com>
2023-10-20 07:27:05 -0300

    Alterações no menu de navegação.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="tw-bg-header">
+  <nav class="tw-bg-header tw-font-roboto">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
@@ -17,13 +17,16 @@
 
 nav {
   padding: 30px;
+  font-size: 1.5rem;
 
   a {
     font-weight: bold;
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      transition: 0.2s cubic-bezier(0.46, 0.03, 0.52, 0.96);
+      border-bottom: #fff1f2 solid 2px;
+      @apply tw-text-text;
     }
   }
 }
