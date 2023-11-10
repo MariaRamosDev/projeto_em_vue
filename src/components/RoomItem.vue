20eae34355cf0659123fe9083c6197f35f765dd6
igorbkuhl <igorb.kuhl@gmail.com>
2023-11-09 23:33:49 -0300

    Chamada do axios removida

--- src/components/RoomItem.vue
+++ src/components/RoomItem.vue
@@ -1,7 +1,6 @@
 <!-- eslint-disable vue/require-default-prop -->
 <script lang="ts">
 import { defineComponent } from "vue";
-import axios from "axios";
 
 export default defineComponent({
   name: "RoomItem",
