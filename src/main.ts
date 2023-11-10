28a6db88f4da2c587203254e9970511a1e5460ac
igorbkuhl <igorb.kuhl@gmail.com>
2023-11-09 23:31:08 -0300

    Bootstrap removido

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
-import "bootstrap/dist/css/bootstrap.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/tailwind.css";
 
 createApp(App).use(router).mount("#app");
-
-import "bootstrap/dist/js/bootstrap.js";
