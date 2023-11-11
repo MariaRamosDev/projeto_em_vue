a41302cce7fa01e1d18bb2eeebf1139c72197b8b
igorbkuhl <igorb.kuhl@gmail.com>
2023-11-11 13:38:05 -0300

    Informações recebidas do MainPage.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,36 @@
-<template>
-  <MainPage />
-</template>
-
 <script lang="ts">
 import { defineComponent } from "vue";
-import MainPage from "@/components/MainPage.vue";
+import RoomItem from "@/components/RoomItem.vue";
+import axios from "axios";
 
 export default defineComponent({
-  name: "HomeView",
+  name: "HoveView",
   components: {
-    MainPage,
+    RoomItem,
+  },
+  data() {
+    return {
+      rooms: [],
+    };
+  },
+  async beforeMount() {
+    this.rooms = await axios
+      .get("http://localhost:3000/rooms")
+      .then((response) => response.data)
+      .catch((error) => console.log(`Algo deu errado 🫤👉 ${error}`));
   },
 });
 </script>
+
+<template>
+  <article>
+    <RoomItem
+      v-for="room in rooms"
+      :key="room.id"
+      :title="room.title"
+      :description="room.description"
+      :price="room.price"
+      :img="room.img"
+    />
+  </article>
+</template>
